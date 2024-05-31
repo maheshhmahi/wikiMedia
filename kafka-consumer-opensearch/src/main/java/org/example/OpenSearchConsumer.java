c01b00173cbb9bbcc45b692fff5353a4b8c1f395
mahesh <maheshh.mahii.98@gmail.com>
2024-05-31 15:39:55 +0530

    UPDATED: made the consumer commit strategy to be false and added logic

--- kafka-consumer-opensearch/src/main/java/org/example/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/org/example/OpenSearchConsumer.java
@@ -75,6 +75,7 @@ public class OpenSearchConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         // create the producer
         return new KafkaConsumer<>(properties);
@@ -135,6 +136,10 @@ public class OpenSearchConsumer {
 
                     }
                 }
+
+                // commit offsets after the batch is consumed
+                consumer.commitAsync();
+                log.info("Offsets have been committed");
 
             }
         }
